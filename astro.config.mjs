9283c22e5663656523889979e6f442bef1d24874
ackenney <ackenney@gmail.com>
2024-11-07 14:38:28 -0500

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,16 @@ import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
 
+const SERVER_PORT = 3000;
+// the url to access your blog during local development
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+// the url to access your blog after deploying it somewhere (Eg. Netlify)
+const BASE_URL = "https://ackenney.github.io";
+// this is the astro command your npm script runs
+
+
 // https://astro.build/config
 export default defineConfig({
+  site: BASE_URL,
   integrations: [tailwind()],
 });
